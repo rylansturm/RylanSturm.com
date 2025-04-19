169a235286e6300853a7f4e82717dcff33656813
Rylan Sturm <rylan.sturm@gmail.com>
2025-04-18 21:40:09 -0700

    added flask shell parameters inside rylansturm.py

--- rylansturm.py
+++ rylansturm.py
@@ -1,1 +1,23 @@
-from app import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+from app.models import User
+from app.disney_models import (
+    AttractionType,
+    Attraction,
+    Entry,
+    Park,
+)
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'sa': sa,
+        'so': so,
+        'db': db,
+        'User': User,
+        'AttractionType': AttractionType,
+        'Attraction': Attraction,
+        'Entry': Entry,
+        'Park': Park,
+    }
